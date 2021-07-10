3f389433c945e7794dc3438151eb74fb47b719dd
pimpbot9000 <t.valimaki@gmail.com>
2021-07-10 20:25:29 +0300

    Add readme.MD and clean up stuff

--- src/index.js
+++ src/index.js
@@ -5,17 +5,11 @@ import { Provider } from 'react-redux'
 import App from './App'
 import { BrowserRouter as Router } from "react-router-dom"
 
-import { shallowEqual } from 'react-redux'
 
-const a = {"b": "tuomas", "c": "heikki"}
-const c = {"b": "tuomas", "c": "heikki"}
-console.log("are equal", c == a)
 ReactDOM.render(
   <Router>
-  <Provider store={store}>
-    
-      <App />
-    
+  <Provider store={store}>    
+      <App />    
   </Provider>
   </Router>
   ,
